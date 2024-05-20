542375b23fd6201f7ec0bee4340218e26d898f35
Julien <berge.jul@gmail.com>
2024-05-20 22:00:52 +0200

    add button reservation

--- demeure_insolite/src/App.tsx
+++ demeure_insolite/src/App.tsx
@@ -1,24 +1,30 @@
-import dilogo from "./assets/demeureinsolite_logo.svg";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import dilogo from './assets/demeureinsolite_logo.svg'
+import './App.css'
 
 function App() {
-	return (
-		<>
-			<div>
-				<img src={dilogo} className="logo" alt="Vite logo" />
-			</div>
-			<h1>Bienvenue sur le site officiel de Demeure insolite</h1>
-			<div className="card">
-				<button>Réserver</button>
-				<p>
-					Bientot vous pourrez réserver nos logement a Gruissan ou dans les
-					Corbières directement depuis le site sans intermédiare et au meilleur
-					prix
-				</p>
-			</div>
-			<p className="read-the-docs">demeureinsolite@gmail.com</p>
-		</>
-	);
+ 
+
+  return (
+    <>
+      <div>
+          <img src={dilogo} className="logo" alt="Vite logo" />
+      </div>
+      <h1>Bienvenue sur le site officiel de Demeure insolite</h1>
+      <div className="card">
+        <button>
+          Réserver
+        </button>
+        <p>
+          Bientot vous pourrez réserver nos logement a Gruissan ou dans les Corbières directement depuis le site sans intermédiare et au meilleur prix
+        </p>
+      </div>
+      <p className="read-the-docs">
+        demeureinsolite@gmail.com
+      </p>
+    </>
+  )
 }
 
 export default App;
