3c0d9db99f9f1404ab6a28fe74caf4fea289cf60
Julien <berge.jul@gmail.com>
2024-05-22 20:17:11 +0200

    Add GA

--- demeure-insolite/app/page.js
+++ demeure-insolite/app/page.js
@@ -1,5 +1,3 @@
-'use client'; // Indiquer que ce composant est un composant côté client
-
 import Image from 'next/image';
 import {
   Card,
@@ -9,48 +7,9 @@ import {
   CardHeader,
   CardTitle,
 } from '../components/ui/card';
-import { useEffect } from 'react';
 
 export default function Home() {
-  useEffect(() => {
-    const loadScript = (src, callback) => {
-      const script = document.createElement('script');
-      script.src = src;
-      script.async = true;
-      script.onload = callback;
-      document.head.appendChild(script);
-    };
-
-    // Charger jQuery et jQueryUI
-    loadScript('https://code.jquery.com/jquery-3.6.0.min.js', () => {
-      loadScript('https://code.jquery.com/ui/1.12.1/jquery-ui.min.js', () => {
-        // Charger le widget
-        loadScript(
-          'https://media.xmlcal.com/widget/1.00/js/bookWidget.min.js',
-          () => {
-            // Initialiser le widget une fois que tout est chargé
-            jQuery(document).ready(function () {
-              jQuery('#bookWidget-122356-233352-0-1716330485').bookWidget({
-                propid: 233352,
-                formAction: 'https://beds24.com/booking.php',
-                widgetLang: 'fr',
-                widgetType: 'BookingBox',
-                borderColor:'#ffffff',
-                boxShadow:false,
-                backgroundColor:'#fcfcfc',
-
-              });
-            });
-          }
-        );
-      });
-    });
-
-    // Nettoyer les scripts après démontage du composant
-    return () => {
-      // Supprimer les scripts chargés si nécessaire
-    };
-  }, []);
+  
 
   return (
     <main className='flex flex-col items-center'>
