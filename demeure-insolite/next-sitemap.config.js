e236d7ea4a3e0059f0d671d57d01718b0956eb5b
Julien <berge.jul@gmail.com>
2024-05-23 20:19:48 +0200

    version mini fonctionnelle

---
+++ demeure-insolite/next-sitemap.config.js
@@ -0,0 +1,7 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://www.demeureinsolite.fr', // Remplacez par l'URL de votre site
+    generateRobotsTxt: true, // (optionnel) génère aussi un fichier robots.txt
+    // options supplémentaires ici
+  };
+  
