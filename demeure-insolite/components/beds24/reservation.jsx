2a92c4cbba4306ffb2c14af6cc93204dab091214
Julien <berge.jul@gmail.com>
2024-05-23 19:09:53 +0200

    modif css widget res

--- demeure-insolite/components/beds24/reservation.jsx
+++ demeure-insolite/components/beds24/reservation.jsx
@@ -1,6 +1,8 @@
 'use client'; // Indiquer que ce composant est un composant côté client
 
 import { useEffect } from 'react';
+import { faBed } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Reservation() {
   useEffect(() => {
@@ -16,23 +18,28 @@ export default function Reservation() {
     loadScript('https://code.jquery.com/jquery-3.6.0.min.js', () => {
       loadScript('https://code.jquery.com/ui/1.12.1/jquery-ui.min.js', () => {
         // Charger le widget
-        loadScript(
-          'https://media.xmlcal.com/widget/1.00/js/bookWidget.min.js',
-          () => {
-            // Initialiser le widget une fois que tout est chargé
-            jQuery(document).ready(function () {
-              jQuery('#bookWidget-122356-233352-0-1716330485').bookWidget({
-                propid: 233352,
-                formAction: 'https://beds24.com/booking.php',
-                widgetLang: 'fr',
-                widgetType: 'BookingBox',
-                borderColor: '#ffffff',
-                boxShadow: false,
-                backgroundColor: '#fcfcfc',
-              });
+        loadScript('/bookWidget.min.js', () => {
+          // Initialiser le widget une fois que tout est chargé
+          jQuery(document).ready(function () {
+            jQuery('#bookWidget-122356-0-0-1716470854').bookWidget({
+              ownerid: 122356,
+              borderColor: '#ffffff',
+              boxShadow: false,
+              buttonBackgroundColor: '#88743a',
+              dateFormat: 'MM d, yy',
+              dateSelection: 1,
+              defaultNumAdult: 2,
+              defaultNumNight: 7,
+              formAction: 'https://beds24.com/booking.php',
+              fontSize: '14px',
+              peopleSelection: 1,
+              showLabels: true,
+              weekFirstDay: 1,
+              widgetLang: 'fr',
+              widgetType: 'BookingStrip',
             });
-          }
-        );
+          });
+        });
       });
     });
 
@@ -43,8 +50,12 @@ export default function Reservation() {
   }, []);
 
   return (
-    <div>
-      <div id='bookWidget-122356-233352-0-1716330485'></div>
+    <div className=' my-32 pb-10 w-1/2  bg-card'>
+      <h2 className=' pb-4 pt-14 text-center font-medium text-primary'>
+        <FontAwesomeIcon icon={faBed} className=' mr-2 h-4' />
+        RÉSERVEZ VOTRE SÉJOUR
+      </h2>
+      <div id='bookWidget-122356-0-0-1716470854'> </div>
     </div>
   );
 }
