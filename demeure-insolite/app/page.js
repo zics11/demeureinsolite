f1d2d58816b15e200874424f4f8b58e7a0f3a48f
Julien <berge.jul@gmail.com>
2024-05-23 20:19:48 +0200

    version mini fonctionnelle

--- demeure-insolite/app/page.js
+++ demeure-insolite/app/page.js
@@ -9,25 +9,33 @@ import {
 } from '../components/ui/card';
 import Reservation from '../components/beds24/reservation';
 import Footer from '../components/section/footer';
+import { NextSeo } from 'next-seo';
+
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center '>
-      <Image
-        src='/DI_logo.svg'
-        width={500}
-        height={500}
-        className='mt-16'
-        alt='Logo'
-      />
-      <h1 className='text-3xl font-bold'>
-        Hébergements atypiques et expériences authentiques dans le Sud de la
-        France.
-      </h1>
-      <section className="mt-14  flex w-full justify-center bg-paysage bg-[url('/paysage-durban.jpeg')] bg-cover bg-center pb-10 pt-10 mb-36">
-            <Reservation></Reservation>
-      </section>
-      <Footer></Footer>
-    </main>
+    <>
+      {/* <NextSeo
+        title='Accueil - Demeure Insolite'
+        description="Page d'accueil de Demeure Insolite. Découvrez nos hébergements uniques en Occitanie."
+      /> */}
+      <main className='flex flex-col items-center '>
+        <Image
+          src='/DI_logo.svg'
+          width={500}
+          height={500}
+          className='mt-16'
+          alt='Logo'
+        />
+        <h1 className='text-3xl font-bold'>
+          Hébergements atypiques et expériences authentiques dans le Sud de la
+          France.
+        </h1>
+        <section className="bg-paysage  mb-36 mt-14 flex w-full justify-center bg-[url('/paysage-durban.jpeg')] bg-cover bg-center pb-10 pt-10">
+          <Reservation></Reservation>
+        </section>
+        <Footer></Footer>
+      </main>
+    </>
   );
 }
---
+++ demeure-insolite/next-seo.config.js
@@ -0,0 +1,17 @@
+// next-seo.config.js
+export default {
+    title: 'Hébergements Insolites en Occitanie - Villas, Cabane, Tipi et Plus | Demeure Insolite',
+    description: 'Découvrez nos hébergements insolites en Occitanie : villas, cabanes, tipis, et plus. Profitez du soleil, des plages, des sports nautiques et de la gastronomie locale dans un cadre authentique et naturel. Réservez dès maintenant !',
+    openGraph: {
+      url: 'https://dhanar98.hashnode.dev/',
+      title: 'Titre Open Graph',
+      description: 'Description Open Graph',
+      site_name: 'Dhanar98',
+    },
+    twitter: {
+      handle: '@dhanar98',
+      site: '@dhanar98',
+      cardType: 'summary_large_image',
+    }
+  };
+  
